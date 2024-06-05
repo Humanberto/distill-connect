01193c2ff1a9ad523977de87336c2f4d04f14290
Roberto Leitao <humanberto@gmail.com>
2024-06-04 20:44:47 -0700

    More bugs with filter and sort

--- app.py
+++ app.py
@@ -205,64 +205,106 @@ Probably only about 20% is my work. But I did type it all in order to get used t
 # 
 # =============================================================================
 '''
-from flask import Flask, render_template, request, redirect, url_for
-import report_manager
-from report_manager import save_reports, load_reports, create_report, get_report, delete_report
+# from flask import Flask, render_template, request, redirect, url_for
+# import report_manager
+# from report_manager import save_reports, load_reports, create_report, get_report, delete_report
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def home():
+#     return render_template('home.html')
+
+# @app.route('/reports')
+# def view_reports():
+#     return render_template('view_reports.html', reports=report_manager.reports)
+
+# @app.route('/report/<int:report_id>', methods=['GET', 'POST'])
+# def report_detail(report_id):
+#     report = get_report(report_id)
+    
+#     if request.method == 'POST':
+#         if 'update' in request.form:
+#             for i, row in enumerate(report['details'][1:], start=1):
+#                 par = request.form.get(f'par-{i}', type=int, default=0)
+#                 pos = request.form.get(f'pos-{i}', type=int, default=0)
+#                 report['details'][i][6] = par
+#                 report['details'][i][7] = pos
+#                 report['details'][i][8] = report['details'][i][4] - par - pos                
+#             save_reports()
+#         elif 'sort' in request.form:
+#             sort_by = request.form['sort']
+#             sort_index = report['details'][0].index(sort_by)
+#             report['details'] = [report['details'][0]] + sorted(report['details'][1:], key=lambda x: x[sort_index])
+#         elif 'filter' in request.form:
+#             filter_term = request.form['filter']
+#             report['details'] = [report['details'][0]] + [row for row in report['details'][1:] if filter_term.lower() in str(row).lower()]
+            
+#     return render_template('report_detail.html', report=report)
+
+# @app.route('/create', methods=['GET', 'POST'])
+# def create_report_route():
+#     if request.method == 'POST':
+#         file = request.files['file']
+#         if file:
+#             report, report_id = create_report(file)
+#             if report:
+#                 return redirect(url_for('report_detail', report_id=report_id))
+#         else:
+#             return "Error processing file", 500
+#     return render_template('create_report.html')
+
+# @app.route('/instructions')
+# def instructions():
+#     return render_template('instructions.html')
+
+# @app.route('/report/<int:report_id>/delete', methods=['POST'])
+# def delete_report_route(report_id):
+#     report_manager.delete_report(report_id)
+#     return redirect(url_for('view_reports'))
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+
+
+'''
+# =============================================================================
+# Version 4
+# 
+# =============================================================================
+'''
+
+
+from flask import Flask, request, jsonify
+import json
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('home.html')
+# Load reports (assuming the reports are loaded here)
+with open('reports.json', 'r') as f:
+    reports = json.load(f)
 
-@app.route('/reports')
-def view_reports():
-    return render_template('view_reports.html', reports=report_manager.reports)
+@app.route('/update_report/<int:report_id>', methods=['POST'])
+def update_report(report_id):
+    data = request.json
+    rowIndex = data['rowIndex']
+    par = data['par']
+    pos = data['pos']
+    total = data['total']
 
-@app.route('/report/<int:report_id>', methods=['GET', 'POST'])
-def report_detail(report_id):
-    report = get_report(report_id)
-    
-    if request.method == 'POST':
-        if 'update' in request.form:
-            for i, row in enumerate(report['details'][1:], start=1):
-                par = request.form.get(f'par-{i}', type=int, default=0)
-                pos = request.form.get(f'pos-{i}', type=int, default=0)
-                report['details'][i][6] = par
-                report['details'][i][7] = pos
-                report['details'][i][8] = report['details'][i][4] - par - pos                
-            save_reports()
-        elif 'sort' in request.form:
-            sort_by = request.form['sort']
-            sort_index = report['details'][0].index(sort_by)
-            report['details'] = [report['details'][0]] + sorted(report['details'][1:], key=lambda x: x[sort_index])
-        elif 'filter' in request.form:
-            filter_term = request.form['filter']
-            report['details'] = [report['details'][0]] + [row for row in report['details'][1:] if filter_term.lower() in str(row).lower()]
-            
-    return render_template('report_detail.html', report=report)
+    # Update the report details
+    for report in reports:
+        if report['id'] == report_id:
+            report['details'][rowIndex + 1][5] = par  # Update PAR
+            report['details'][rowIndex + 1][6] = pos  # Update POs
+            report['details'][rowIndex + 1][7] = total  # Update Total
 
-@app.route('/create', methods=['GET', 'POST'])
-def create_report_route():
-    if request.method == 'POST':
-        file = request.files['file']
-        if file:
-            report, report_id = create_report(file)
-            if report:
-                return redirect(url_for('report_detail', report_id=report_id))
-        else:
-            return "Error processing file", 500
-    return render_template('create_report.html')
+    # Save the updated reports back to the file
+    with open('reports.json', 'w') as f:
+        json.dump(reports, f)
 
-@app.route('/instructions')
-def instructions():
-    return render_template('instructions.html')
-
-@app.route('/report/<int:report_id>/delete', methods=['POST'])
-def delete_report_route(report_id):
-    report_manager.delete_report(report_id)
-    return redirect(url_for('view_reports'))
+    return jsonify({'status': 'success'})
 
 if __name__ == '__main__':
     app.run(debug=True)
-
